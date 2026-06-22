9d27db7cb55c14a73c94a6e83881598fcf0c5f4c
Prajwal G <prajwalgprajju12@gmail.com>
2026-06-22 17:56:00 +0530

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,38 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/HomePage";
-import PublicProfile from "./pages/PublicProfile";
-import FaceVerify from "./pages/FaceVerify";
+import { lazy, Suspense } from "react";
 
 import "./index.css";
 
+const Home = lazy(() => import("./pages/HomePage"));
+const PublicProfile = lazy(() => import("./pages/PublicProfile"));
+const FaceVerify = lazy(() => import("./pages/FaceVerify"));
 
 export default function App() {
   return (
     <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/face-verify" element={<FaceVerify />} />
-        <Route path="/u/:userId" element={<PublicProfile />} />
-        <Route path="*" element={<div>Page Not Found</div>} />
-      </Routes>
+      <Suspense
+        fallback={
+          <div
+            style={{
+              height: "100vh",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              fontSize: "18px",
+              fontWeight: "600",
+            }}
+          >
+            Loading...
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/face-verify" element={<FaceVerify />} />
+          <Route path="/u/:userId" element={<PublicProfile />} />
+          <Route path="*" element={<div>Page Not Found</div>} />
+        </Routes>
+      </Suspense>
     </HashRouter>
   );
 }
